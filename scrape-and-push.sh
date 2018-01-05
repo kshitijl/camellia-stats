734d9907c63f0f6d1c8a5c90c10513414cff9f54
Kshitij Lauria <kslauria@gmail.com>
2018-01-04 23:36:51 -0500

    Update cron script

--- scrape-and-push.sh
+++ scrape-and-push.sh
@@ -1,4 +1,4 @@
 ./webcomic_stats_scraper/scrape-camellia-stats.py --config /home/kslauria/production-config.json download-and-measure
-./webcomic_stats_scraper/scrape-camellia-stats.py --config /home/kslauria/production-config.json generate-report
+./webcomic_stats_scraper/scrape-camellia-stats.py --config /home/kslauria/production-config.json generate-reports
 git commit -am "Scheduled scrape"
 git push
